6424630ae2bbb4c147bf588ec4599e45ab1819e4
Mehrdad <mehrdadscomputer@gmail.com>
2018-06-28 00:53:40 +0430

    the broadcast receiver works but not on Android 8

--- app/src/main/java/ir/mehrdadscomputer/receiver/CustomBroadcastReceiver.java
+++ app/src/main/java/ir/mehrdadscomputer/receiver/CustomBroadcastReceiver.java
@@ -3,17 +3,12 @@ package ir.mehrdadscomputer.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.text.TextUtils;
 import android.widget.Toast;
 
-public class CustomBroadcastReceiver extends BroadcastReceiver{
+public class CustomBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        String action = intent.getAction();
-        if("ir.mehrdadscomputer.broadcast.CUSTOM_BROADCAST".equals(action)) {
-            // When receive it will show an toast popup message.
-            Toast.makeText(context, "Custom Broadcast Receiver Receive Message.", Toast.LENGTH_SHORT).show();
-        }
+        Toast.makeText(context, "Action: " + intent.getAction(), Toast.LENGTH_SHORT).show();
     }
 }
