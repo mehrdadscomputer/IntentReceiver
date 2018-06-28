8c547a6c139739316aa883f9998025709f203f25
Mehrdad <mehrdadscomputer@gmail.com>
2018-06-29 00:34:28 +0430

    Now broadcastReceiver works in Android 8

--- app/src/main/java/ir/mehrdadscomputer/receiver/CustomBroadcastReceiver.java
+++ app/src/main/java/ir/mehrdadscomputer/receiver/CustomBroadcastReceiver.java
@@ -3,12 +3,52 @@ package ir.mehrdadscomputer.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 
 public class CustomBroadcastReceiver extends BroadcastReceiver {
 
+    List<String> mList = new ArrayList<>();
+
+    public static final String CUSTOM_BROADCAST_ACTION2 = "test2";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Action: " + intent.getAction(), Toast.LENGTH_SHORT).show();
+
+        createListOfStrings();
+
+        // Get intent, action and MIME type
+        String type = intent.getType();
+
+        String Result = handleSendText(intent); // Handle text being sent
+        Log.i("ttt2", "result: " + Result);
+
+        // send message to receiver
+        Intent intent2 = new Intent();
+        intent2.setAction("customreceiveraction");
+        intent2.putExtra("message", Result);
+        context.sendBroadcast(intent2);
+
+        }
+
+    String handleSendText(Intent intent) {
+        String sharedText = intent.getStringExtra("receiverTestValue");
+        Log.i("ttt0", "sharedText: " + sharedText);
+        if (mList.contains(sharedText)) return "Yes";
+        else return "No";
+    }
+
+    private void createListOfStrings() {
+        mList.add("ir.example.test1");
+        mList.add("ir.example.test2");
+        mList.add("ir.example.test3");
+        mList.add("ir.example.test4");
+        mList.add("ir.example.test5");
     }
 }
